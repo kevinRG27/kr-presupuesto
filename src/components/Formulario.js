1d0bbc7feb90fba5ec756dd7388032ab151f9e00
Kevin <kalonso.restrepo@udea.edu.co>
2021-04-26 23:19:19 -0500

    Components terminate

---
+++ src/components/Formulario.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+import Error from './Error'
+import shortid from 'shortid'
+import PropTypes from 'prop-types'
+
+
+const Formulario = ({guardarGasto,guardarCrearGasto}) => {
+
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+        //Cuando el usuario agrega un gasto
+        const agregarGasto = (e) => {
+        e.preventDefault();
+
+        //Validar gastos
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim()===''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        //Construir gasto (objeto)
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        }
+
+        //Pasar gasto al componente principal
+        guardarGasto(gasto);
+        guardarCrearGasto(true);
+
+        //Limpiar Formulario
+        guardarNombre('');
+        guardarCantidad(0);
+
+    }
+    return (  
+        <form
+            onSubmit={agregarGasto}
+        >
+            <h2>Agrega tus gastos aquí</h2>
+
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto incorrecto"/> : null}
+
+            <div className="campo">
+                <label>Nombre Gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={(e) => guardarNombre(e.target.value)}
+                />
+            </div>
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 18000"
+                    value={cantidad}
+                    onChange={(e) => guardarCantidad(parseInt(e.target.value))}
+                />
+            </div>
+
+            <input 
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar Gasto"
+            />
+        </form>
+    );
+}
+
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired 
+}
+export default Formulario;
--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,15 +1,47 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+import Error from './Error'
+import PropTypes from 'prop-types'
 
-const Pregunta = () => {
-    return (
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+
+//Creando el State
+const [cantidad, guardarCantidad] = useState(0);
+const [error,guardarError] = useState(false);
+
+//Funcion para validar el presupuesto ingresado
+const definirPresupuesto = (e) =>{
+    guardarCantidad(parseInt(e.target.value),10)
+}   
+
+//Submit para  definir Presupuesto
+const agregarPresupuesto = (e) => {
+    e.preventDefault();
+
+    if(cantidad < 1 || isNaN(cantidad)){
+        guardarError(true);
+        return
+    }
+    guardarError(false);
+    guardarPresupuesto(cantidad);
+    guardarRestante(cantidad);
+    actualizarPregunta(false);
+
+}
+
+return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
+            {error ? <Error mensaje="El presupuesto es incorrecto"/> : null}
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input 
                 type="number"
                 className="u-full-width"
                 placeholder="Coloca tu presupuesto"
+                onChange={definirPresupuesto}
                 />
                 <input
                 type="submit"
@@ -21,5 +53,10 @@ const Pregunta = () => {
         </Fragment>
       );
 }
- 
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired 
+}
 export default Pregunta;
