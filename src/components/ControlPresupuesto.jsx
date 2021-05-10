c471011b77a414d86a5d34df747097af54cbc5f7
Kevin Restrepo G <58571762+kevinRG27@users.noreply.github.com>
2021-05-10 18:40:35 -0500

    Change sintaxis

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -3,20 +3,19 @@ import { revisarPresupuesto} from '../helpers'
 import PropTypes from 'prop-types'
 
 const ControlPresupuesto = ({presupuesto, restante}) => {
-    console.log(restante};
            
     return( 
-    <Fragment>
-        <div className="alert alert-primary">
-            Presupuesto: {presupuesto}
-        </div>
-        <div className={revisarPresupuesto(presupuesto,restante)}>
-            
-            Restante: {restante}
-        </div>
-    </Fragment>
- )
-};
+        <Fragment>
+            <div className="alert alert-primary">
+                Presupuesto: {presupuesto}
+            </div>
+            <div className={revisarPresupuesto(presupuesto,restante)}>
+
+                Restante: {restante}
+            </div>
+        </Fragment>
+     );
+}
 
 ControlPresupuesto.propTypes = {
     presupuesto: PropTypes.number.isRequired,
